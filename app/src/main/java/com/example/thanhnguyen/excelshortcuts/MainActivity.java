20855b21a11a72736be24dfea1ec96a987b31c92
ndthanh <ndt181@gmail.com>
2015-03-01 21:56:47 +0100

    Fix theme issue, try to hide actionbar but not successful

--- app/src/main/java/com/example/thanhnguyen/excelshortcuts/MainActivity.java
+++ app/src/main/java/com/example/thanhnguyen/excelshortcuts/MainActivity.java
@@ -1,33 +1,44 @@
 package com.example.thanhnguyen.excelshortcuts;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v4.widget.SearchViewCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.SearchView;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SearchView;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements ViewTreeObserver.OnScrollChangedListener{
 
     ArrayList<Shortcut> shortcutList;
     CustomAdapter ca;
     EditText searchBox;
+    private float mActionBarHeight;
+    private ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.activity_main);
+
+        final TypedArray styledAttributes = getTheme().obtainStyledAttributes(
+            new int[] {android.R.attr.actionBarSize});
+        mActionBarHeight = styledAttributes.getDimension(0,0);
+        styledAttributes.recycle();
+        mActionBar = getActionBar();
 
         MyDBHandler db = new MyDBHandler(this);
 
@@ -46,6 +57,8 @@ public class MainActivity extends ActionBarActivity {
         shortcutList = db.getAllShortcuts();
 
         ListView shortcutListView = (ListView) findViewById(R.id.shortcutListView);
+        shortcutListView.getViewTreeObserver().addOnScrollChangedListener(this);
+
         ca = new CustomAdapter(this,0,shortcutList);
         shortcutListView.setAdapter(ca);
 
@@ -54,13 +67,16 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem searchItem = menu.findItem(R.id.search);
 
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-        searchView.setIconifiedByDefault(false);
-
-        int id = android.support.v7.appcompat.R.id.search_src_text;
-        searchBox = (EditText) searchView.findViewById(id);
+        if (Build.VERSION.SDK_INT >= 11) {
+            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setIconifiedByDefault(false);
+            searchView.setQueryHint("Search shortcuts");
+            int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+            searchBox = (EditText) searchView.findViewById(id);
+        }
 
         searchBox.addTextChangedListener(new TextWatcher() {
             @Override
@@ -72,22 +88,12 @@ public class MainActivity extends ActionBarActivity {
                     ca.resetData();
                 }
                 ca.getFilter().filter(s.toString());
-
             }
 
             @Override
             public void afterTextChanged(Editable s) {}
         });
 
-        if (Build.VERSION.SDK_INT >= 11) {
-            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-
-            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
-            searchView.setQueryHint("Search shortcuts");
-        } else {
-            SearchViewCompat.setSearchableInfo(searchView, getComponentName());
-        }
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -104,5 +110,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onScrollChanged() {
+        float y = ((ListView)findViewById(R.id.shortcutListView)).getScrollY();
+        if ( y >= mActionBarHeight && mActionBar.isShowing()) {
+            mActionBar.hide();
+        } else if (y == 0 && !mActionBar.isShowing()) {
+            mActionBar.show();
+        }
     }
 }
